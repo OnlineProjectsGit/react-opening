2490dcc3b2bfee40812455163cbe8aeeee82ffcb
OnlineProjectsGit <emtech.projects.live@gmail.com>
2024-06-17 14:13:21 +1000

    Fixed push error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import SayHi from './components/HiComponent';
+import NavBar from './components/NavBar';
+import Users from './components/Users';
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.users = [
+      { name: "Michael", age: 24, email: "Michael@email.com" },
+      { name: "Michaela", age: 35, email: "Michaela@email.com" },
+      { name: "Michaelason", age: 46, email: "Michaelason@email.com" }
+    ]
+    this.state = {
+      user: this.users[1].name
+    }
+
+    this.updateUser = this.updateUser.bind(this)
+  }
+
+  updateUser() {
+    let newNum = Math.floor(Math.random() * 3)
+    let newName = this.users[newNum].name
+    this.setState({ user: newName })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <NavBar />
+
+        <SayHi
+          name={"The Team"}
+        />
+
+        <div>
+          <p>{this.state.user}</p>
+          <button onClick={this.updateUser}>Click Me!</button>
+        </div>
+
+        <br />
+
+        <Users />
+
+      </div>
+    )
+  }
 }
 
-export default App;
+
